2fe4073bd591695282e51450f04b1709bd87ac58
pratham aggarwal <prathamaggarwal23@gmail.com>
2022-11-20 15:35:50 +0530

    navbar updates

--- pages/components/navbar/Navbar.jsx
+++ pages/components/navbar/Navbar.jsx
@@ -1,32 +1,45 @@
 import styles from './navbar.module.css';
 
-import { signout, auth  } from 'utils/firebase.utils';
+import { signout, auth, database  } from 'utils/firebase.utils';
 import Button from 'react-bootstrap/Button';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { onValue, ref} from 'firebase/database';
+import { useEffect, useState } from 'react';
 
 export default function Navbar () {
 
-    const [user, loading] = useAuthState(auth);
+    const [currentuser, setUser] = useState(null);
+    const [user, loading ] = useAuthState(auth);
+    useEffect(() => {
+        if(user)
+        onValue(ref(database, `users/`), snapshot => {
+            if(snapshot.exists()) {
+                setUser(snapshot.val());
+            }
+        })
+
+    },[])
+
+
 
     return (
         <div className={styles.navbarContainer}>
-            <h3><span style={{ maxWidth: '33%' }}>Time elapsed</span></h3>
             <div className = { styles.tokenContainer}>
-                <p className={styles.largeText}>
-                    { user ? (
-                        <h3>Your User Token: {user.uid}<br></br></h3> 
-                    )  : <h4>Sign in</h4>}
-                </p>
+                <span className={styles.largeText}>
+                    { currentuser ? (
+                        <h4>Players in lobby: {5 - currentuser.vacancy}</h4> 
+                    )  : <h4>Sign in To play</h4>}
+                </span>
             </div>
             <h3>
                 {
                     user ? 
                     <Button 
-                variant="primary" 
-                size='lg'
-                onClick = {() => signout()}
-                >Sign Out</Button> :
-                <h1>🙋‍♂️</h1>
+                    variant="primary" 
+                    size='lg'
+                    onClick = {() => signout()}
+                    >Sign Out</Button> :
+                    <h1>🙋‍♂️</h1>
                 }
             </h3>
         </div>
